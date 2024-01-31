cd7f3f0dbb7ec20011e8fa98b1c31e82e424ca62
nehadadel <eng.nehadadel@gmail.com>
2024-01-31 15:18:03 -0800

    task1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -9,7 +9,7 @@
 */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-binary_tree_t left_node, tmp = NULL;
+binary_tree_t *left_node, *tmp = NULL;
 if (parent == NULL)
 {
 return (NULL); }
